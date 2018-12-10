b7f91344b9975f00a465761efa40126f27d934af
Zhanchong Chen <czbug.cn@gmail.com>
2018-12-10 21:52:43 +0800

    read the config file via system env

--- conf/config.go
+++ conf/config.go
@@ -2,15 +2,24 @@ package conf
 
 import (
 	"github.com/spf13/viper"
+	"os"
 )
 
 func LoadConfig() *viper.Viper {
+	MODE := os.Getenv("MODE")
+
 	v := viper.New()
 
 	v.AddConfigPath("./conf/")
-
-	v.SetConfigName("config")
 	v.SetConfigType("yaml")
+
+	if MODE == "PRODUCTION" {
+		v.SetConfigName("production-config")
+	} else if MODE == "DEVELOPMENT" {
+		v.SetConfigName("development-config")
+	} else {
+		v.SetConfigName("config")
+	}
 
 	if err := v.ReadInConfig(); err != nil {
 		return nil
