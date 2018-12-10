19b857475e615f63b116d1b1d06bf1241d8d502c
Zhanchong Chen <czbug.cn@gmail.com>
2018-12-10 21:36:43 +0800

    logging config

--- app/app.go
+++ app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 var Server *echo.Echo
@@ -10,6 +11,8 @@ var Server *echo.Echo
 // init echo web server
 func Init() {
 	Server = echo.New()
+
+	Server.Logger.SetLevel(log.INFO)
 	// Middleware
 	Server.Use(middleware.Logger())
 	Server.Use(middleware.Recover())
